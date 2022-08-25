ecc0755efa4835c7b0bda1eb85ebdbdb1ebc8176
Felix Arjuna <79026094+felixarjuna@users.noreply.github.com>
2022-08-25 11:54:04 +0200

    Update install.sh

--- backend/scripts/install.sh
+++ backend/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "$(git rev-parse --show-toplevel)" || (
+cd "$(git rev-parse --show-toplevel)" && cd "backend" || (
   echo "Couldn't find project folder. Please check your working dir."
   exit 1
 )
